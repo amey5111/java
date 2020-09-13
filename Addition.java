924a60478fd6598da51e42a8866fe9a19baf949a
amey5111 <71187037+amey5111@users.noreply.github.com>
2020-09-13 13:47:35 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,13 @@
+//Java program for addition of two numbers
+public class Addition {
+
+    
+    public static void main (String args[]){
+//Variables requred
+        int num1= 33;
+        int num2= 55;
+        int sum= num1+num2 ;
+        System.out.println("Addition of given two numbers is:"+sum);
+    }
+
+}
